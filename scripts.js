b4197656804284a9d5c0c8f08ad32f9b9cbc539a
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-08-12 11:53:10 -0600

    Changed layout of navbar, added iframe to load projects

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
+function setIframeHeight(iframe) {
+	if (iframe) {
+		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+		if (iframeWin.document.body) {
+			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+		}
+	}
+};
+
 $(document).ready(function(){
+
+  var exit = '<div id="exit"><i class="fa fa-times-circle" aria-hidden="true"></i></div>';
+
+  $('.navigation li a').on('click', function(){
+		$('#frame').removeClass('hidden');
+    $('#frame').html(exit + '<iframe src="http://kimberly.codes/FreeCodeCamp/' + this.id + '"></iframe>');
+    setIframeHeight(document.getElementById('#frame'));
+
+		$('#exit').on('click', function(){
+			$('#frame').addClass('hidden');
+		});
+  });
+
 
 
 });

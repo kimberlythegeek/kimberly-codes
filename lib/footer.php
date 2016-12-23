6e8d28ef8ec14dbfd3e45e1e3d8025ced731f6a3
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-12-23 14:26:43 -0700

    added links

--- lib/footer.php
+++ lib/footer.php
@@ -9,6 +9,8 @@
     <li><a href="http://github.com/kimberlythegeek"><i class="fa fa-github" aria-hidden="true"></i></a></li>
     <li><a href="http://codepen.io/kimberlythegeek/pens/public"><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
     <li><a href="http://linkedin.com/in/kannepennington"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+    <li><a href="https://www.facebook.com/kimberlythegeek/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+    <li><a href="http://www.kimberlythegeek.com/"><i class="fa fa-wordpress" aria-hidden="true"></i></a></li>
   </ul>
 </footer>
 </html>

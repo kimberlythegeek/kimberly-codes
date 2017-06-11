a10dab314af3e7d8f4229a3810351d1f34326268
root <root@ip-172-26-8-208.us-west-2.compute.internal>
2017-06-11 20:07:15 +0000

    update

---
+++ lib/footer.php
@@ -0,0 +1,16 @@
+</div><!-- .row -->
+</div><!-- .container-fluid -->
+</body>
+
+<footer>
+  <ul class="footer-nav">
+    <li><a href="http://freecodecamp.com/kimberlythegeek"><i class="fa fa-free-code-camp" aria-hidden="true"></i></a></li>
+    <li><a href="http://twitter.com/kimberlythegeek"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+    <li><a href="http://github.com/kimberlythegeek"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+    <li><a href="http://codepen.io/kimberlythegeek/pens/public"><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
+    <li><a href="http://linkedin.com/in/kannepennington"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+    <li><a href="https://www.facebook.com/kimberlythegeek/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+    <li><a href="http://www.kimberlythegeek.com/"><i class="fa fa-wordpress" aria-hidden="true"></i></a></li>
+  </ul>
+</footer>
+</html>
---
+++ lib/header.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch:400|Great+Vibes|Catamaran:900|Fredericka+the+Great|Oswald:400" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/e1862a0f00.js"></script>
+    <script src="scripts.js"></script>
+
+    <?php include_once("analytics.php") ?>
+
+    <link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+    <title> < kimberly /> </title>
+    <meta name="developer portfolio" content="kimberly.codes | Kimberly Pennington | Web Designer and Developer Portfolio" />
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
+  </head>
+
+  <header>
+  </header>
+
+  <body>
+    <div class="container-fluid">
+      <div id="page" class="row">
